a8a57684c595589eca750f94dc34ef4497404c1f
Sreehari Inukollu <hari.inukollu@ttu.edu>
2017-07-25 16:44:41 -0500

    Finisehd the stack with linked list implementation.

--- StackQueues/src/com/isreehari/algm/StackQueues.java
+++ StackQueues/src/com/isreehari/algm/StackQueues.java
@@ -30,6 +30,7 @@ public class StackQueues {
             userChoice = inputScanner.nextInt();            
             switch(userChoice){
                 case 1: thisStackQueues.stackImplementationArray(inputScanner); break;
+                case 2: thisStackQueues.stackImplementationLinkedList(inputScanner); break;
                 case 0: inputScanner.close(); System.exit(0); break;
                 default: System.out.println("Wrong choice"); break;
             }
@@ -65,6 +66,36 @@ public class StackQueues {
                   case 0: System.exit(0); break; 
                   default: System.out.println("Wrong choice"); break;
               }
+        }
+    }
+    
+    public void stackImplementationLinkedList(Scanner inputScanner){
+        int userChoice, newValue;
+        StackLinkedList thisStackLinkedList = new StackLinkedList();
+        while(true){
+            System.out.println("************** Stack implementaion with Linked List **************");
+            System.out.println("1. Push an element on the stack");
+            System.out.println("2. Pop an element from the stack");
+            System.out.println("3. Display the top element in the stack");    
+            System.out.println("4. Display all the stack elements");
+            System.out.println("5. Display size of the stack");
+            System.out.println("6. Return Main Menu");                        
+            System.out.println("0. Quit the application");
+            System.out.println("************** /Stack implementaion with Linked List **************");
+            System.out.print("Please enter your choice: ");
+            userChoice = inputScanner.nextInt();
+            switch(userChoice){
+                case 1: System.out.print("Please enter new value: ");
+                         newValue = inputScanner.nextInt();
+                         thisStackLinkedList.push(newValue); break;
+                case 2: thisStackLinkedList.pop(); break;
+                case 3: System.out.println("Peek of the stack is : "+ thisStackLinkedList.peek()); break;
+                case 4: thisStackLinkedList.displayAll(); break;
+                case 5: System.out.println("Size of the stack is : "+thisStackLinkedList.size()); break;
+                case 6: return;
+                case 0: System.exit(0); break;
+                default: System.out.println("Wrong choice");                         
+            }
         }
     }
     
