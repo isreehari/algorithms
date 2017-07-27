2a1af40cffb0b8432e605bf67209947e185e29a1
Sreehari Inukollu <hari.inukollu@ttu.edu>
2017-07-27 17:05:25 -0500

    Stated Deque with circular array.

--- StackQueues/src/com/isreehari/algm/StackQueues.java
+++ StackQueues/src/com/isreehari/algm/StackQueues.java
@@ -27,6 +27,7 @@ public class StackQueues {
             System.out.println("3. Queue implementation with Array");
             System.out.println("4. Queue implementation with Linked List");
             System.out.println("5. Queue implementation with Circular Linked List");
+            System.out.println("6. Queue implementation with Circular Array");
             System.out.println("0. Quit the Application");
             System.out.println("**************  /Main Menu **************");
             System.out.print("Please enter your choice: ");
@@ -37,6 +38,7 @@ public class StackQueues {
                 case 3: thisStackQueues.queueImplementationArray(inputScanner); break;
                 case 4: thisStackQueues.queueImplementationLinkedList(inputScanner); break;
                 case 5: thisStackQueues.queueImplementationCircularLinkedList(inputScanner);break;
+                case 6: thisStackQueues.queueImplementationCircularArray(inputScanner); break;
                 case 0: inputScanner.close(); System.exit(0); break;
                 default: System.out.println("Wrong choice"); break;
             }
@@ -250,6 +252,46 @@ public class StackQueues {
                             }
                             
                       
+                  case 6: return; 
+                  case 0: System.exit(0); break; 
+                  default: System.out.println("Wrong choice"); break;
+              }
+        }
+        
+    }
+     public void queueImplementationCircularArray(Scanner inputScanner){
+        int userChoice, newValue;
+        QueueCircularArray thisQueueCircularArray = new QueueCircularArray(4);
+        while(true){
+              System.out.println("************** Queue implementation with Array **************");
+              System.out.println("1. Insert an element on the queue");
+              System.out.println("2. Delete an element from the queue");
+              System.out.println("3. Display the front element in queue");
+              System.out.println("4. Display all the elements in the queue");
+              System.out.println("5. Display size of the queue");
+              System.out.println("6. Return Main Menu");
+              System.out.println("0. Quit the application");
+              System.out.println("************** /Stack implementation with Array **************");              
+              System.out.print("Please enter your choice: ");
+              userChoice = inputScanner.nextInt();
+              switch(userChoice){
+                  case 1:  
+                           System.out.print("Please enter new value :");
+                           newValue = inputScanner.nextInt();
+                           thisQueueCircularArray.insert(newValue); break; 
+                  case 2:  thisQueueCircularArray.delete(); break;
+                  case 3:  System.out.println("Peek of queue is: "+ thisQueueCircularArray.peek()); break; 
+                  case 4:  
+                           if(thisQueueCircularArray.isEmpty()){                               
+                               System.out.print("Opps! The Queue is empty."); 
+                           }else{
+                                System.out.print("The Queue is: ["); 
+                                thisQueueCircularArray.display(); 
+                                System.out.print("]\n");
+                           }
+                           
+                           break;
+                  case 5:  System.out.println("Size of the queue is: "+ thisQueueCircularArray.size()); break;                  
                   case 6: return; 
                   case 0: System.exit(0); break; 
                   default: System.out.println("Wrong choice"); break;
