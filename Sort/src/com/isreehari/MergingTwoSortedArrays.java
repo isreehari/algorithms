6a30aa37218c1221ba3b25827df6a30c4ef304ae
Hari, Inukollu <hari.inukollu@ttu.edu>
2017-08-25 12:50:52 -0500

    Finished merge with same sorted array.

--- Sort/src/com/isreehari/MergingTwoSortedArrays.java
+++ Sort/src/com/isreehari/MergingTwoSortedArrays.java
@@ -15,13 +15,13 @@ public class MergingTwoSortedArrays {
     
     public static void main(String[] args){
         Scanner inputScanner = new Scanner(System.in);
+        MergingTwoSortedArrays mergingTwoSortedArrays = new MergingTwoSortedArrays();
         int arraySize1 = inputScanner.nextInt();
         int[] givenArray1 = new int[arraySize1];
         for(int index = 0; index < arraySize1; index++){
             givenArray1[index] = inputScanner.nextInt();
         }
         
-        MergingTwoSortedArrays mergingTwoSortedArrays = new MergingTwoSortedArrays();
         givenArray1 = mergingTwoSortedArrays.doInsertionSort(givenArray1, arraySize1);
         
         int arraySize2 = inputScanner.nextInt();
@@ -35,13 +35,23 @@ public class MergingTwoSortedArrays {
         
         
         
-        int tempArraySize = arraySize1 + arraySize2;
-        int[] tempArray = new int[tempArraySize];
-        tempArray = mergingTwoSortedArrays.doMerge(givenArray1, givenArray2, tempArray);
+        int tempArraySize1 = arraySize1 + arraySize2;
+        int[] tempArray1 = new int[tempArraySize1];
+        tempArray1 = mergingTwoSortedArrays.doMerge(givenArray1, givenArray2, tempArray1);
         
-        for(int index = 0; index < tempArraySize; index++){
-            System.out.print(tempArray[index] + " ");
+        for(int index = 0; index < tempArraySize1; index++){
+            System.out.print(tempArray1[index] + " ");
         }
+        
+        
+        int tempArraySize2 = 6;
+        int[] tempArray2 = {2,6,10, 4,5,7};
+        tempArray2 = mergingTwoSortedArrays.doMergeWithSingleArray(tempArray2,0,2,3,5);
+        
+        for(int index = 0; index < tempArraySize2; index++){
+            System.out.print(tempArray2[index] + " ");
+        }
+        
         
         System.out.println();
         
@@ -87,6 +97,31 @@ public class MergingTwoSortedArrays {
         
         
         return temp;
+    } 
+    
+     public int[] doMergeWithSingleArray(int temp[],int low1, int up1, int low2, int up2){
+        
+        int i = low1, j = low2, currentIndex = 0;
+        int[] temp2 = new int[temp.length];
+        
+        
+           while(i <= up1 && j <= up2 ){
+                if(temp[i] > temp[j]){
+                    temp2[currentIndex++] = temp[j++];                    
+                }else{
+                    temp2[currentIndex++] = temp[i++];
+                }
+            }
+        
+        
+        while(i <= up1)
+            temp2[currentIndex++] = temp[i++];
+        
+        while(j <= up2)
+            temp2[currentIndex++] = temp[j++];
+        
+        
+        return temp2;
     } 
     
 }
