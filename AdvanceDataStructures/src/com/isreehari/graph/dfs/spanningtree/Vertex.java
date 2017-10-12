8f6b40729d7e9f19f78870109585b61a8c82bb32
Hari, Inukollu <hari.inukollu@ttu.edu>
2017-10-12 17:42:38 -0500

    Finished the DFS.

---
+++ AdvanceDataStructures/src/com/isreehari/graph/dfs/spanningtree/Vertex.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.isreehari.graph.dfs.spanningtree;
+
+/**
+ *
+ * @author sinukoll
+ */
+public class Vertex {
+    public String name;
+    public int state;
+    public int predecessor;
+    
+    public Vertex(String newName){
+        this.name = newName;
+    }
+    
+    @Override
+    public String toString(){
+        return this.name;
+    }
+}
